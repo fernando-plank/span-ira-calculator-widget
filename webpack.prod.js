cb861f381f666f667bc6d2783fb6621a42cd3c73
Fernando Rocha <fernando.rocha@joinplank.com>
2023-05-18 10:51:39 -0300

    update webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,12 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
       '@routes': path.resolve(__dirname, 'src/routes'),
+      "@assets": path.resolve(__dirname, 'src/assets'),
+      "@pages": path.resolve(__dirname, 'src/pages'),
     },
   },
   output: {
