3b44ee077aee4ad42e04cd5e980c7d61de6b7b08
Fernando Rocha <fernando.rocha@joinplank.com>
2023-05-19 10:39:29 -0300

    include loading and reference to scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-
-import React, {useEffect, useMemo, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import HouseholdInformation from '@components/HouseholdInformation'
 import LocalRebates from '@components/LocalRebates'
 
@@ -15,49 +14,49 @@ export type IncentivesProps = {
 }
 
 export const Home = () => {
-    const [incentives, setIncentives] = useState<any>();
-    const [taxCreditsInformation, setTaxCreditsInformation] =
-        useState<TaxCreditsTableData[]>([])
-    const [houseHoldingInformation, setHouseHoldingInformation] =
-        useState<IncentivesProps>()
+  const [incentives, setIncentives] = useState<any>()
+  const [taxCreditsInformation, setTaxCreditsInformation] = useState<
+    TaxCreditsTableData[]
+  >([])
+  const [houseHoldingInformation, setHouseHoldingInformation] =
+    useState<IncentivesProps>()
+  const incentivesRef = useRef(null)
+  const executeScroll = () => incentivesRef.current.scrollIntoView()
+  const fetchIncentives = async () => {
+    const { incentives } = await new IncentivesServices().getIncentives()
+    return incentives
+  }
 
-    const fetchIncentives = async() => {
-        const { incentives } = await new IncentivesServices().getIncentives()
-        return incentives
-    }
+  useEffect(() => {
+    const items = incentives?.filter((item) => item.type === 'tax_credit')
+    setTaxCreditsInformation(items)
+  }, [incentives])
 
-    useEffect(() => {
-        const items = incentives?.filter((item) => item.type === 'tax_credit');
-        setTaxCreditsInformation(
-            items
-        )
-    }, [incentives])
+  useEffect(() => {
+    fetchIncentives().then((incentives) => {
+      setIncentives(incentives)
+    })
+  }, [])
+  const onSubmitCallback = (data) => setHouseHoldingInformation(data)
 
-    useEffect(() => {
-        fetchIncentives().then(incentives => {
-            setIncentives(incentives);
-        })
-    }, [])
-
-    const onSubmitCallback = (data) => setHouseHoldingInformation(data)
-
-    return (
-        <>
-            {/*<Navbar/>*/}
-            {/*<BannerHeroSection/>*/}
-            <HouseholdInformation
-                info={houseHoldingInformation}
-                onSubmitCallback={onSubmitCallback}
-            />
-            <PersonalizedIncentives householdInformation={houseHoldingInformation}/>
-            <TaxCredits tableData={taxCreditsInformation}/>
-            <LocalRebates/>
-            {/*<Faq/>*/}
-            {/*<BannerGetQuote/>*/}
-            {/*<About/>*/}
-            {/*<Footer/>*/}
-        </>
-    )
+  return (
+    <>
+      {/*<Navbar/>*/}
+      {/*<BannerHeroSection/>*/}
+      <HouseholdInformation
+        executeScroll={executeScroll}
+        info={houseHoldingInformation}
+        onSubmitCallback={onSubmitCallback}
+      />
+      <PersonalizedIncentives incentivesRef={incentivesRef} householdInformation={houseHoldingInformation} />
+      <TaxCredits tableData={taxCreditsInformation} />
+      <LocalRebates />
+      {/*<Faq/>*/}
+      {/*<BannerGetQuote/>*/}
+      {/*<About/>*/}
+      {/*<Footer/>*/}
+    </>
+  )
 }
 
 export default Home
