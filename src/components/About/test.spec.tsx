0f378d35bbace7694c0b0d4d4310817fce31e180
GuilhermeSantosPlank <guilherme.coelho@joinplank.com>
2023-05-22 18:19:09 -0300

    Tests coverage

--- src/components/About/test.spec.tsx
+++ src/components/About/test.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '../../utils/test-utils'
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 
 import About from '.'
 
@@ -10,5 +10,18 @@ describe('<About />', () => {
     expect(screen.getByTestId('about-title')).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should call open on click button', () => {
+    const { container } = render(<About />)
+    const openSpy = jest.spyOn(window, 'open')
+
+    const readBlogButton = screen.getByTestId('read-blog-button')
+    expect(readBlogButton).toBeInTheDocument()
+
+    fireEvent.click(readBlogButton)
+
+    expect(container.firstChild).toMatchSnapshot()
+    expect(openSpy).toBeCalled()
   })
 })
