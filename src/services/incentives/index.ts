e61aa74851bb28cdc7af764d839f6c44109168be
GuilhermeSantosPlank <guilherme.coelho@joinplank.com>
2023-05-25 14:49:17 -0300

    Removing mock url

--- src/services/incentives/index.ts
+++ src/services/incentives/index.ts
@@ -6,11 +6,11 @@ import { IncentivesObject } from '../types'
 class IncentivesServices {
   getIncentives: () => Promise<IncentivesObject> = async () => {
     const response = await fetch.get<IncentivesObject>(
-      'https://api.rewiringamerica.org/api/v0/incentives',
+      apiRoutes.incentives,
       null,
       {
         headers: {
-          Authorization: `Bearer zpka_36dd9115f1b2463b93857ea0c517e640_73f9ee2a`
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_REWIRING_AMERICA_TOKEN}`
         }
       }
     )
