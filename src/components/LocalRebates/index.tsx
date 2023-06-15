d55313f7538a701680130ce3a14f0199f7208e98
GuilhermeSantosPlank <guilherme.coelho@joinplank.com>
2023-06-15 16:40:40 -0300

    Link

--- src/components/LocalRebates/index.tsx
+++ src/components/LocalRebates/index.tsx
@@ -34,7 +34,7 @@ const LocalRebates = () => {
                   <S.LearnMoreButton
                     onClick={() => {
                       window.open(
-                        'https://www.siliconvalleypower.com/home/showpublisheddocument/77952/637945284994130000',
+                        'https://www.irs.gov/credits-deductions/frequently-asked-questions-about-energy-efficient-home-improvements-and-residential-clean-energy-property-credits',
                         '_blank'
                       )
                     }}
